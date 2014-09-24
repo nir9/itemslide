960f77a4e0325606ceee499ad63df05b1752d3e7
nirl <nir.lich@gmail.com>
2014-09-24 23:28:11 +0300

    some changes

--- README.md
+++ README.md
@@ -2,3 +2,12 @@ itemslide.github.io
 ===================
 
 jQuery plugin for item sliding
+
+Dependinces:
+jQuery
+Hammer.js
+
+How to initialize:
+
+$("ul").initslide();
+
--- src/itemslide.js
+++ src/itemslide.js
@@ -1,47 +1,70 @@
-(function($){
+(function ($) {
     var accel = 0;
     var overallslide = 0;
-    var sensitivity = 10;
+    var sensitivity = 20;
     var slides;
     var currentIndex = 0;
-	$.fn.initslide = function (){
-		
-		//var target = $(this).attr('id');
-		//alert(target);
+    //Waypoints check position relative to waypoint and decide if to scroll to or not...
+    $.fn.initslide = function () {
+
+        //var target = $(this).attr('id');
+        //alert(target);
         //$(this)
-        
-        slides = $(this);//Saves the object in a variable //.children
+
+        slides = $(this); //Saves the object in a variable //.children
         //alert("children: " + slides.length);
         /*$( this ).children( 'li.target' ).css("border", "3px double red");*/
         //alert(slides.children.length*slides.children('li').css("width").replace("px",""));
-        console.log(slides.css("width"));//TODO: solve width problem
+        console.log(slides.css("width")); //TODO: solve width problem
         /*slides.css("width",slides.children.length*slides.children('li').css("width").replace("px","")*2);*/
-        $('li:nth-child('+(currentIndex+1)+')').attr('id','active');
-        
-        var mc = new Hammer(slides.get(0));//Retrieve DOM Elements to create hammer.js object
-        
-        mc.on("panleft panright", function(ev) {
-//            console.log(ev.velocityX +" gesture detected.");
-            overallslide -= ev.velocityX*sensitivity;
-            
-            slides.css("left",overallslide);
+        $('li:nth-child(' + (currentIndex + 1) + ')').attr('id', 'active');
+
+        var mc = new Hammer(slides.get(0)); //Retrieve DOM Elements to create hammer.js object
+
+        mc.on("panleft panright", function (ev) {
+            //            console.log(ev.velocityX +" gesture detected.");
+            overallslide -= ev.velocityX * sensitivity;
+
+            slides.css("left", overallslide);
         });
-         mc.on("swipe", function(ev) {
-//            console.log(ev.velocityX +" gesture detected.");
-            accel -= ev.velocityX/100;//TODO: Solve acceleration problem
+        mc.on("swipe", function (ev) {
+            //            console.log(ev.velocityX +" gesture detected.");
+            /*accel -= ev.velocityX/100;
             while(accel>0)
             {
                 console.log("YAYY");
                 overallslide+=accel;
                 slides.css("left",overallslide);
                 accel-=0.001;
-            }
+            }*/
+            
+            console.log(ev.velocityX);
+            slides.animate({
+                left: "-="+(ev.velocityX*100)
+            }, {
+                    duration: 250,
+                    easing: 'swing'
+                });
+            
+            //$(this).next();
             //slides.css("left",overallslide);
         });
-        
-            /*overallslide += accel;
+
+        /*overallslide += accel;
             accel-=0.01;*/
-        
-	}
+
+    }
+
+
+    $.fn.next = function () { //Next slide
+        changeActiveSlideTo(currentIndex + 1);
+    }
+
+    function changeActiveSlideTo(i) {
+        $('li:nth-child(' + (currentIndex + 1) + ')').attr('id', '');
+        currentIndex = i;
+        console.log("new index: " + currentIndex);
+        $('li:nth-child(' + (currentIndex + 1) + ')').attr('id', 'active');
+    }
 
 })(jQuery);
