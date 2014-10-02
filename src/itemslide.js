e33e678254630c4d79ec30166a4e7ed8e608d403
nir9 <nir_lichtman@hotmail.com>
2014-10-02 18:53:00 +0300

    some fixes and changes...

--- src/itemslide.js
+++ src/itemslide.js
@@ -1,7 +1,7 @@
 (function ($) {
     var accel = 0;
     var overallslide = 0;
-    var sensitivity = 20;
+    var sensitivity = 10;
     var slides;
     var currentIndex = 0;
     //Waypoints check position relative to waypoint and decide if to scroll to or not...
@@ -12,48 +12,53 @@
         //$(this)
 
         // (WIDTH of (this) - WIDTH of slide)/2
-        
+
         slides = $(this); //Saves the object given to the plugin in a variable
-        
-        
-        
+
+
+
         console.log(slides.css("width"));
-        
-        slides.css("left", ($("body").css("width").replace("px","")-slides.css("left").replace("px","")-slides.children('li').css("width").replace("px",""))/2);//Centerize sliding area
-        
+
+        slides.css("left", ($("body").css("width").replace("px", "") - slides.css("left").replace("px", "") - slides.children('li').css("width").replace("px", "")) / 2); //Centerize sliding area
+
         console.log(slides.css("left"));
-        
-        
+
+
         $('li:nth-child(' + (currentIndex + 1) + ')').attr('id', 'active');
 
         var mc = new Hammer(slides.get(0)); //Retrieve DOM Elements to create hammer.js object
+        var disable = false;
+        mc.on("panleft panright", function (ev) { //Hammerjs pan(drag) event happens very fast
 
-        mc.on("panleft panright", function (ev) {//Hammerjs pan(drag) event happens very fast
-            
-
-            slides.css("left", "-="+ev.velocityX);//Change x of slides to velocity of drag
+            console.log("YAYY");
+            if (!disable) {
+                slides.css("left", "-=" + ev.velocityX * sensitivity); //Change x of slides to velocity of drag
+            } else {
+                disable = false;
+            }
         });
-        mc.on("swipe", function (ev) {//Hammerjs Swipe (called when mouse realsed after mouse down)
-            
-            
+        mc.on("swipe", function (ev) { //Hammerjs Swipe (called when mouse realsed after mouse down)
+
+
             console.log(ev.velocityX);
-            
-            
-            
+
+
+
             slides.animate({
-                left: "-="+(ev.velocityX*250)
+                left: "-=" + (ev.velocityX * 250)
             }, {
-                    /*duration: 225,*/
-                    easing: 'easeOutQuart'//Choose easing from easing plugin http://gsgd.co.uk/sandbox/jquery/easing/
-                });
-            
-            
-        },{
-            velocity: 0.05,//minimum velocity
-            threshold: 0//Minimum distance
+                /*duration: 225,*/
+                easing: 'easeOutQuart' //Choose easing from easing plugin http://gsgd.co.uk/sandbox/jquery/easing/
             });
 
-        
+            disable = true;
+
+        }, {
+            velocity: 0.05, //minimum velocity
+            threshold: 0 //Minimum distance
+        });
+
+
 
     }
 
