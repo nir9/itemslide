3244e29b9a7d375c4ee38e9bcdf0dae8c090134f
nir9 <nir_lichtman@hotmail.com>
2014-10-02 07:55:27 +0300

    Update itemslide.js

--- src/itemslide.js
+++ src/itemslide.js
@@ -57,7 +57,7 @@
                 left: "-="+(ev.velocityX*250)
             }, {
                     /*duration: 225,*/
-                    easing: 'easeInSine'
+                    easing: 'easeInExpo'
                 });
             
             //$(this).next();
