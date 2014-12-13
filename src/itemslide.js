0f576555177cf8e105b667c6a55cdf90246e4192
Nir Lichtman <nir_lichtman@hotmail.com>
2014-12-13 22:13:44 +0200

    Improved springy panning on edges and cleaned code.

--- src/itemslide.js
+++ src/itemslide.js
@@ -15,44 +15,48 @@
 
     $.fn.initslide = function (options) {
 
+        var initialLeft = 0;
+
+        //Animation variables
+        var countFrames = 0;
+        var currentPos = 0;
+        var currentLandPos = 0;
+        var slidesGlobalID = 0; //rAf id
+
+
         var direction = 0; //Panning Direction
         var isBoundary = false; //Is current slide the first or last one
         var distanceFromStart = 0;
 
-        //Includes ItemSlide variables so that they will be individual to each object that is applied with itemslide.
-        var defaults = {
+
+
+        var defaults = { //Options
             duration: 200,
             swipe_sensitivity: 150,
             disable_slide: false,
-            disable_autowidth: false,
             disable_scroll: false,
             start: 0,
             one_item: false, //Set true for full screen navigation or navigation with one item every time
             pan_threshold: 0.3, //Precentage of slide width
-
-            //Until here options
-
-            currentIndex: 0,
-            currentPos: 0,
-            begin: 0,
-            targetFrame: 0,
-            countFrames: 0,
-            currentLandPos: 0,
-            initialLeft: 0,
-            slidesGlobalID: 0
-
+            disable_autowidth: false
 
         };
 
         var settings = $.extend({}, defaults, options);
 
 
-        this.data("settings", settings);
+        this.data("vars", //Variables that can be accessed publicly
+            {
+                currentIndex: 0,
+                disable_autowidth: settings.disable_autowidth
+            });
+
+
 
 
         var slides = $(this); //Saves the object given to the plugin in a variable
 
-        slides.data("settings").initialLeft = parseInt(slides.css("left").replace("px", ""));
+        initialLeft = parseInt(slides.css("left").replace("px", ""));
 
 
         slides.css({ //Setting some css to avoid problems on touch devices
@@ -105,7 +109,7 @@
 
                 isDown = 1;
 
-                prevent = 0;//to know when to start prevent default
+                prevent = 0; //to know when to start prevent default
 
                 startPoint = touch.pageX;
 
@@ -139,25 +143,36 @@
             }
         });
 
+        //MouseMove related variables
+        var firstTime = true;
+        var secondStartPt = 0;
+        var savedpos = 0;
 
         function mousemove(e) //Called by mousemove event (inside the mousedown event)
         {
 
 
-
+            //Check type of event
             if (e.type == 'touchmove') //Check for touch event or mousemove
+            {
                 touch = e.originalEvent.touches[0] || e.originalEvent.changedTouches[0];
-            else
+
+                if (Math.abs(touch.pageX - startPoint) > 10) //If touch event than check if to start preventing default behavior
+                    prevent = 1;
+
+                if (prevent)
+                    e.preventDefault();
+
+            }
+            else //Regular mousemove
+            {
                 touch = e;
-
-
-
-
-            if(touch.pageX - startPoint > 10 || touch.pageX - startPoint < -10)//These are for the preventDefault
-                prevent=1;
-
-            if(prevent)
                 e.preventDefault();
+            }
+
+
+
+
 
 
 
@@ -166,16 +181,9 @@
             slides.trigger('pan');
 
 
-            slides.translate3d((touch.pageX - startPoint) / (isOutBoundaries() ? 4 : 1)
-                //The shorthand ifs are to check if on one of the boundaries and if yes than check which direction is out of range to apply 1/4 of pan.
-
-                + slides.data("settings").currentLandPos);
 
 
-
-
-
-
+            //Set direction of panning
             if ((-(touch.pageX - startPoint)) > 0) { //Set direction
                 direction = 1; //PAN LEFT
             } else {
@@ -183,8 +191,38 @@
             }
 
 
+
+
+            //If out boundaries than set some variables to save previous location before out boundaries
+            if(isOutBoundaries())
+            {
+
+                if(firstTime){
+                    secondStartPt = touch.pageX;
+                    savedpos = (touch.pageX - startPoint);
+                    firstTime = 0;
+                }
+
+            }
+            else{
+                firstTime = 1;
+            }
+
+            //Reposition according to current deltaX
+            slides.translate3d(
+
+                    ((firstTime==0) ? (savedpos + (touch.pageX - secondStartPt) / 4) : (touch.pageX - startPoint) ) //Check if out of boundaries - if true than add springy panning effect
+
+                                   + currentLandPos);
+
+
+
+
+
+
+
             if ((touch.pageX - startPoint) * direction < 12 * (-1)) //Check to see if TAP or PAN by checking using the tap threshold (if surpassed than cancelAnimationFrame and start panning)
-                cancelAnimationFrame(slides.data("settings").slidesGlobalID); //STOP animation of sliding because if not then it will not reposition according to panning if animation hasn't ended
+                cancelAnimationFrame(slidesGlobalID); //STOP animation of sliding because if not then it will not reposition according to panning if animation hasn't ended
         }
 
 
@@ -215,7 +253,7 @@
                         velocity = -(touch.pageX - startPoint) / deltaTime;
 
                         if (velocity > 0) { //Set direction
-                            direction = 1;
+                            direction = 1; //PAN LEFT
                         } else {
                             direction = -1;
                         }
@@ -235,7 +273,7 @@
                             gotoSlideByIndex(getLandingSlideIndex(velocity * settings.swipe_sensitivity - slides.translate3d()));
                             //NOT HERE - remove before commit
                         } else {
-                            if (savedSlide.index() != slides.data("settings").currentIndex) //TODO: SOLVE MINOR ISSUE HERE
+                            if (savedSlide.index() != slides.data("vars").currentIndex) //TODO: SOLVE MINOR ISSUE HERE
                             { //If this occurs then its a tap
                                 e.preventDefault(); //FIXED
                                 gotoSlideByIndex(savedSlide.index());
@@ -258,9 +296,9 @@
         try {
             slides.mousewheel(function (event) {
 
-                if (!slides.data("settings").disable_scroll) {
+                if (!settings.disable_scroll) {
                     velocity = 0;
-                    var mouseLandingIndex = slides.data("settings").currentIndex - event.deltaY;
+                    var mouseLandingIndex = slides.data("vars").currentIndex - event.deltaY;
 
                     if (mouseLandingIndex >= slides.children('li').length || mouseLandingIndex < 0) //If exceeds boundaries dont goto slide
                         return;
@@ -287,14 +325,14 @@
 
 
 
-            slides.children(':nth-child(' + (slides.data("settings").currentIndex + 1) + ')').attr('id', ''); //WORKS!!
+            slides.children(':nth-child(' + (slides.data("vars").currentIndex + 1) + ')').attr('id', ''); //WORKS!!
 
 
             slides.children(':nth-child(' + (i + 1) + ')').attr('id', 'active'); //Change destination index to active
 
             if (i != settings.currentIndex) //Check if landingIndex is different from currentIndex
             {
-                settings.currentIndex = i; //Set current index to landing index
+                slides.data("vars").currentIndex = i; //Set current index to landing index
                 slides.trigger('changeActiveIndex');
             }
 
@@ -310,7 +348,7 @@
 
                 if (slides.children(i).width() * i + slides.children(i).width() / 2 -
 
-                    slides.children(i).width() * slides.data("settings").pan_threshold * direction - getPositionByIndex(0) > x) {
+                    slides.children(i).width() * settings.pan_threshold * direction - getPositionByIndex(0) > x) {
 
 
                     if (!settings.one_item)
@@ -318,10 +356,10 @@
 
                     else //If one item navigation than no momentum therefore different landing slide(one forward or one backwards)
                     {
-                        if (i != slides.data("settings").currentIndex)
-                            return slides.data("settings").currentIndex + 1 * direction //Return 0 or more
+                        if (i != slides.data("vars").currentIndex)
+                            return slides.data("vars").currentIndex + 1 * direction //Return 0 or more
                         else
-                            return slides.data("settings").currentIndex;
+                            return slides.data("vars").currentIndex;
                     }
 
 
@@ -329,19 +367,19 @@
 
             }
 
-            return settings.one_item ? slides.data("settings").currentIndex + 1 : slides.children('li').length - 1; //If one item enabled than just go one slide forward and not until the end.
+            return settings.one_item ? slides.data("vars").currentIndex + 1 : slides.children('li').length - 1; //If one item enabled than just go one slide forward and not until the end.
 
         }
 
 
 
         function getPositionByIndex(i) {
-            return -(i * slides.children('li').width() - ((slides.parent().width() - slides.data("settings").initialLeft - slides.children('li').width()) / 2));
+            return -(i * slides.children('li').width() - ((slides.parent().width() - initialLeft - slides.children('li').width()) / 2));
         }
 
 
         function isOutBoundaries() { //Return if user is panning out of boundaries
-            return (isBoundary && direction == ((slides.getActiveIndex() > 0) ? 1 : (-1)))
+            return (((Math.floor(slides.translate3d())>(getPositionByIndex(0)) && direction == -1 )||(Math.ceil(slides.translate3d())<(getPositionByIndex(slides.children('li').length - 1)) && direction == 1 )));
         }
 
         function gotoSlideByIndex(i) {
@@ -367,28 +405,28 @@
 
 
 
-            slides.data("settings").currentPos = slides.translate3d();
+            currentPos = slides.translate3d();
 
-            slides.data("settings").currentLandPos = getPositionByIndex(i);
-
-
+            currentLandPos = getPositionByIndex(i);
 
 
 
-            slides.data("settings").countFrames = 0;
+
+
+            countFrames = 0;
 
 
             //SET DURATION
 
-            total_duration = Math.max(slides.data("settings").duration
+            total_duration = Math.max(settings.duration
 
                 - ((1920 / $(window).width()) * Math.abs(velocity) *
-                    7 * (slides.data("settings").duration / 230) //Velocity Cut
+                    7 * (settings.duration / 230) //Velocity Cut
 
                 )
 
                 - (isOutBoundaries() ? (distanceFromStart / 15) : 0) // Boundaries Spring cut
-                * (slides.data("settings").duration / 230) //Relative to chosen duration
+                * (settings.duration / 230) //Relative to chosen duration
 
                 , 10
             ); //Minimum duration is 10
@@ -396,8 +434,8 @@
             //SET DURATION UNTILL HERE
 
 
-            cancelAnimationFrame(slides.data("settings").slidesGlobalID);
-            slides.data("settings").slidesGlobalID = requestAnimationFrame(animationRepeat);
+            cancelAnimationFrame(slidesGlobalID);
+            slidesGlobalID = requestAnimationFrame(animationRepeat);
 
 
 
@@ -409,7 +447,7 @@
 
         }
 
-        var total_duration = slides.data("settings").duration;
+        var total_duration = settings.duration;
 
         function animationRepeat() { //Repeats using requestAnimationFrame
 
@@ -422,15 +460,15 @@
             slides.trigger('changePos');
 
 
-            slides.data("settings").currentPos -= easeOutQuart(slides.data("settings").countFrames, 0, slides.data("settings").currentPos - slides.data("settings").currentLandPos, total_duration);
+            currentPos -= easeOutQuart(countFrames, 0, currentPos - currentLandPos, total_duration);
 
 
 
             //to understand easings refer to: http://upshots.org/actionscript/jsas-understanding-easing
 
-            if (Math.round(slides.data("settings").currentPos) == slides.data("settings").currentLandPos) {
+            if (Math.round(currentPos) == currentLandPos) {
 
-                slides.data("settings").countFrames = 0;
+                countFrames = 0;
                 return; //out of recursion
             }
 
@@ -439,11 +477,11 @@
 
 
 
-            slides.translate3d(slides.data("settings").currentPos);
+            slides.translate3d(currentPos);
 
 
-            slides.data("settings").countFrames++;
-            slides.data("settings").slidesGlobalID = requestAnimationFrame(animationRepeat);
+            countFrames++;
+            slidesGlobalID = requestAnimationFrame(animationRepeat);
 
 
         }
@@ -467,21 +505,21 @@
     $.fn.next = function () { //Next slide
 
 
-        this.gotoSlide(this.data("settings").currentIndex + 1);
+        this.gotoSlide(this.data("vars").currentIndex + 1);
 
 
     }
 
     $.fn.previous = function () { //Next slide
 
-        this.gotoSlide(this.data("settings").currentIndex - 1);
+        this.gotoSlide(this.data("vars").currentIndex - 1);
     }
 
     $.fn.reload = function () { //Get index of active slide
-        if (!this.data("settings").disable_autowidth)
+        if (!this.data("vars").disable_autowidth)
             this.css("width", this.children('li').length * this.children('li').width() + 10); //SET WIDTH
 
-        this.gotoSlide(this.data("settings").currentIndex);
+        this.gotoSlide(this.data("vars").currentIndex);
 
     }
 
@@ -497,7 +535,7 @@
 
     //GET
     $.fn.getActiveIndex = function () { //Get index of active slide
-        return this.data("settings").currentIndex;
+        return this.data("vars").currentIndex;
     }
 
     $.fn.getCurrentPos = function () { //Get current position of carousel
@@ -509,11 +547,11 @@
 
     $.fn.translate3d = function (x) //Translates the x of an object or returns the x translate value
     {
-        if (x == null) {
+        if (x != null) {
+            this.css('transform', 'translate3d(' + x + 'px' + ',0px, 0px)');
+        } else {
             var matrix = matrixToArray(this.css("transform"));
             return isExplorer ? parseFloat(matrix[12]) : parseFloat(matrix[4]); //Returns the x value
-        } else {
-            this.css('transform', 'translate3d(' + x + 'px' + ',0px, 0px)');
         }
     }
 
