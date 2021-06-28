b2b2d21577a49663358f0ba6fea4aa6599898d4f
Nir Lichtman <nir@lichtman.org>
2021-06-28 07:50:11 +0300

    Sliding: Remove the parent width adjustment

--- website-src/sliding.js
+++ website-src/sliding.js
@@ -5,17 +5,13 @@ $(document).ready(function () {
     carousel = $("#scrolling ul");
 
     carousel.itemslide({
-        swipe_out: true, //NOTE: REMOVE THIS OPTION IF YOU WANT TO DISABLE THE SWIPING SLIDES OUT FEATURE.
-        remove_deprecated_external_functions: true,
-        parent_width: true
-    }); //initialize itemslide
-
+        swipe_out: true, // NOTE: REMOVE THIS OPTION IF YOU WANT TO DISABLE THE SWIPING SLIDES OUT FEATURE.
+        remove_deprecated_external_functions: true
+    });
 
     $(window).resize(function () {
         carousel.reload();
-    }); //Recalculate width and center positions and sizes when window is resized
-
-
+    });
 
     /* Below are some examples */
 
